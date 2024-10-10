f9af8ec0817413340c4a75eddb1cf56ff4e84df8
PepperMint <joachim.ismael@epitech.eu>
2024-10-10 17:39:40 +0400

    ok

--- my_strlen.c
+++ my_strlen.c
@@ -6,9 +6,12 @@
 */
 
 #include "../../include/my.h"
+#include <stddef.h>
 
 int my_strlen(char const *str)
 {
+    if (str == NULL)
+	return 0;
     int count = 0;
 
     for (int i = 0; str[i] != '\0'; i++) {
