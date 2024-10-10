cc675c418fb8204cc6b759c039b77f4325d50aac
PepperMint <joachim.ismael@epitech.eu>
2024-10-10 09:48:57 +0400

    ok

---
+++ my_isalpha.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** my_isalpha
+** File description:
+** my_isalpha
+*/
+
+#include "my.h"
+
+int my_isalpha(char c)
+{
+    if (('@' < c && c < '[') && ('`' < c && c < '{'))
+	return 0;
+    return 1;
+}
