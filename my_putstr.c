019205a131391570d9e6940cd3a1fc5cb9e7bcc3
PepperMint <joachim.ismael@epitech.eu>
2024-10-11 19:26:06 +0400

    ok

--- my_putstr.c
+++ my_putstr.c
@@ -7,11 +7,10 @@
 
 
 #include "../../include/my.h"
+#include <unistd.h>
 
 int my_putstr(char const *str)
 {
-    for (int i = 0; str[i] != '\0'; i++) {
-        my_putchar(str[i]);
-    }
+    write(1, str, my_strlen(str));
     return 0;
 }
