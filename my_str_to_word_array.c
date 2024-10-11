64bbeb70ab99fb7476e7ed1b999079a8cabdc6d2
PepperMint <joachim.ismael@epitech.eu>
2024-10-11 12:56:23 +0400

    ok

--- my_str_to_word_array.c
+++ my_str_to_word_array.c
@@ -30,15 +30,15 @@ int count_arr(char const *str)
 
 char **my_str_to_word_array(char const *str)
 {
-    int count_arr = count_arr(str);
-    char **arr = malloc(sizeof(char *) * count_arr);
+    int count = count_arr(str);
+    char **arr = malloc(sizeof(char *) * count);
     int arr_index = 0;
     int a = 0;
     int b = 0;
 
     if (str == NULL)
         return arr;
-    while (arr_index != count_arr) {
+    while (arr_index != count) {
         while (my_isalphanum(str[a]) == 1 && str[a] != '\0') {
             a++;
             b++;
