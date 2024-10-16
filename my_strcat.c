39b5611ef47ae5c82e4023b38f58a05686f82820
PepperMint <joachim.ismael@epitech.eu>
2024-10-16 16:46:39 +0400

    ok

---
+++ my_strcat.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** my_strcat
+** File description:
+** my_strcat
+*/
+
+#include "my.h"
+#include <stdlib.h>
+
+char *my_strcat(char const *a, char const *b)
+{
+    int len_a = my_strlen(a);
+    int len_b = my_strlen(b);
+    char *out = malloc(sizeof(char) * (len_a + len_b + 1));
+
+    for (int i = 0; i < len_a; i++) {
+        out[i] = a[i];
+    }
+    for (int i = 0; i < len_b; i++) {
+	out[len_a - 1 + i] = b[i];
+    }
+    out[len_a + len_b - 1] = '\0';
+    return out;
+}
