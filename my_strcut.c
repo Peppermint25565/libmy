3f4431bbfb2760c201380e78398f0a94e8c12f30
PepperMint <joachim.ismael@epitech.eu>
2024-10-16 16:02:59 +0400

    ok

---
+++ my_strcut.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** my_strcut
+** File description:
+** my_strcut
+*/
+
+#include "../../include/my.h"
+
+char *my_strcut(char *str, int index, int len)
+{
+    char *out = malloc(sizeof(char) * (len + 1));
+
+    out[len] = '\0';
+    for (int i = 0; i < len; i++) {
+	out[i] = str[index + i];
+    }
+    return out;
+}
